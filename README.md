dc5ae2abe4faf79cb892e876c434851d931a7647
Chua DongEn <s10202623@connect.np.edu.sg>
2021-01-09 21:13:41 +0800

    final ver, except validation

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# Assignment-2
+# Personal Portfolio
+Name: Chua Dong En<br/>
+Class: P01<br />
+ID: 10202623A<br />
+Website URL: https://chuadongen.github.io/Assignment-2/<br/>
+
+This project is a word search website. This website only has a main page. This website provides the user a quick and easy search for the meanings of a word.
+
+## Warning
+* API use is monthly limited, so please test wisely
+* RapidAPI key is left in js, I don't know how to encrypt it
+* Excessive tests may cost
+
+## Todo
+| Task        | Status           | 
+| ------------- |:-------------:| 
+| Test out api     | done |
+| Append api data to html      | done |
+| Use bootstrap to layout page   |   done   | 
+| Loop image appending |   done    |   
+| Finish making the README.mb |     done  |  
+| Error handling for data and images|   done    |  
+| Media query |   done    |   
+| Text overflow and hover feature |   done    |  
+| Suggestion feature |   done    |  
+## Design Process
+While designing this website, I had to think of my users and how they would use and navigate the website. As a result, I tried to imagine and understand my users from their point of view.
+
+1. A user that is looking for the meaning of a word would want multiple definitions instead of one, therefore I implemented multiple dictionary apis.
+2. A user would also want examples and images of the word, therefore I implemented an image search api.
+
+
+As a web developer, I have a personal "rule" I always follow. That "rule" is to always develop bit size code and always check and ensure that they contain no errors. By developing in this manner, it is easier to troubleshoot and error check, as it is easier to look and detect errors in a small chunk of code than a large chunk of code. This makes developing any software whether is it a website or a programme much easier and faster.
+
+I based my website off my wireframe and as I implemented new features such as search feature and hover feature I corrected my wireframe accordingly.
+
+
+
+## Features
+Since this is a word search website. I have implemented several features to ensure that the user is able to get the needed information as well as some ease of life features.<br/>
+1. Definition and search feature. The feature recieves and displays data from 4 different dictionary apis such as definition, examples, pronounciations and word type.
+2. Image search feature. The search feature also includes images to allow the user to have a better idea of the meaning of the word.
+3. Hover feature. As the length of a definition varies according to the result, I have cut it off with text overflow and overflow. Instead, I implemented a hover feature that increased the height of the dictionary to show the full definitions.
+4. Search suggestion feature. To make the user have a smoother experience, I implemented a search suggestion feature that uses api to display 5 search suggestions for every letter the user enters.
+5. External search feature. In the case when a user wants more results, I have linked external sites such as google or youtube to allow the user to navigate to the external site with the query already searched.
+## Future Features
+1. Make content text clickable to search for text as query, not implemented as I had to change focus to text suggestion feature
+2. Translation feature, not implemented as I could not figure out translation apis
+3. Text to speech or voice feature, not implemented as I could not figure out the apis
+## Testing
+Testing is a vital part in progamming and website development. It ensures that what we are building works and is functional. Therefore, comprehensive testing methods has to be in place to help expose errors.
+### Online validators
+To check and test my website, I used w3school's CSS and Markup service. This allowed me to validate my code and ensure that there are no errors.
+1. W3C CSS Validation Service: https://jigsaw.w3.org/css-validator/
+2. W3C Markup Validation Service: https://validator.w3.org/
+
+### Links
+To test my links, I had to ensure that internal links did not open a new page, while external links all opened a new page when clicked on. This is vital, as it improves the user experience.
+## Credits
+### Icons
+* Google: https://www.pngkit.com/bigpic/u2w7q8q8u2o0o0a9/
+* Youtube: https://www.stickpng.com/img/icons-logos-emojis/social-media-icons/circle-youtube-icon
+* Duckduckgo: https://duckduckgo.com/about
+* Oxford:https://apps.apple.com/sg/app/oxford-advanced-learners-dict/id553809551?mt=12
+* Merriam-Webster: https://commons.wikimedia.org/wiki/File:Merriam-Webster_logo.svg
+* Bing: https://www.iconfinder.com/icons/3069754/bing_circle_round_icon_search_engine_icon
+### Guides
+* StackOverflow
--- script.js
+++ script.js
@@ -1,255 +1,331 @@
-
+// prevent enter for search and run apicall
 $('#search').keypress(function(e){
     if(e.keyCode == 13)
     {
         e.preventDefault();
-        var s1 = document.getElementById("search").value;
-        console.log(s1);
-        fetch("https://lingua-robot.p.rapidapi.com/language/v1/entries/en/"+s1, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
-            "x-rapidapi-host": "lingua-robot.p.rapidapi.com"
-        }
-        })
-        .then(response => response.json()) 
-        .then(function(data){
-            console.log(data); 
-            //clear
-            $('#linguadef').text("");
-            $('#linguadef2').text("");
-            $('#linguapro').text("");
-            $('#linguaexp').text("");
-            $('#linguaexp2').text("");
-            //Insert values
-            //Pronounciation
-            try {
-
-                $('#linguapro').text(data.entries[0].pronunciations[0].context.regions[0]+": "+data.entries[0].pronunciations[0].transcriptions[0].transcription);
-              
-            } catch (err) {
-              
-                $('#linguapro').text("Pronounciation: Not found");
-              
-            }
-            //Definition 1
-            try {
-
-                $('#linguadef').text("1. (" +data.entries[0].lexemes[0].partOfSpeech+") "+data.entries[0].lexemes[0].senses[0].definition);
-              
-            } catch (err) {
-              
-                $('#linguadef').text("Example: Not found");
-              
-            }
-            //Example 1
-            try {
-
-                $('#linguaexp').text("Example: "+data.entries[0].lexemes[0].senses[0].usageExamples[0]);
-              
-            } catch (err) {
-              
-                $('#linguaexp').text("Example: Not found");
-              
-            }
-            //Definition 2
-            try {
-
-                $('#linguadef2').text("2. (" +data.entries[0].lexemes[1].partOfSpeech+") "+data.entries[0].lexemes[1].senses[0].definition);
-              
-            } catch (err) {
-              
-                $('#linguadef2').text("Definition: Not found");
-              
-            }
-            //Example 2
-            try {
-
-                $('#linguaexp2').text("Example. "+data.entries[0].lexemes[1].senses[0].usageExamples[0]);
-              
-            } catch (err) {
-              
-                $('#linguaexp2').text("Example: Not found");
-              
-            }
-
-        });
-    
-        fetch("https://mashape-community-urban-dictionary.p.rapidapi.com/define?term="+s1, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
-            "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
-        }
-        })
-        .then(response => response.json()) 
-        .then(function(data){
-            console.log(data); 
-            //clear
-            $('#urbandef').text("");
-            $('#urbanexp').text("");
-            //Insert values
-            //Definition 1
-            try {
-
-                $('#urbandef').text("1. " +data.list[0].definition);
-              
-            } catch (err) {
-              
-                $('#urbandef').text("Definition: Not found");
-              
-            }
-            //Example 1
-            try {
-
-                $('#urbanexp').text("Example: " +data.list[0].example);
-              
-            } catch (err) {
-              
-                $('#urbanexp').text("Example: Not found");
-              
-            }
-            //Definition 2
-            try {
-
-                $('#urbandef2').text("2. " +data.list[1].definition);
-              
-            } catch (err) {
-              
-                $('#urbandef2').text("Definition: Not found");
-              
-            }
-            //Example 2
-            try {
-
-                $('#urbanexp2').text("Example: " +data.list[1].example);
-              
-            } catch (err) {
-              
-                $('#urbanexp2').text("Example: Not found");
-              
-            }
-
-
-        });
-    
-        fetch("https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry="+s1, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
-                "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com"
-            }
-        })
-        .then(response => response.json()) 
-        .then(function(data){
-            console.log(data); 
-
-            
-            
-            
-            //Noun
-            try {
-                var noun = (data.meaning.noun).split("(nou)");
-                $('#twinnoun').text("1. (nou) " +noun[1]);
-              
-            } catch (err) {
-              
-                $('#twinnoun').text("");
-              
-            }
-            //Verb
-            try {
-                var verb = (data.meaning.verb).split("(vrb)");
-                $('#twinverb').text("2. (vrb)" +verb);
-              
-            } catch (err) {
-              
-                $('#twinverb').text("");
-              
-            }
-            //Adj
-            try {
-                var adj = (data.meaning.adjective).split("(adj)");
-                $('#twinadj').text("3. (adj)" +adj);
-                if (data.meaning.adjective=="") {
-                    $('#twinadj').text("");
-                  }
-              
-            } catch (err) {
-              
-                $('#twinadj').text("");
-              
-            }
-            //Adv
-            try {
-                var adverb = (data.meaning.adverb).split("(adv)");
-                $('#twinadverb').text("4. (adv)" +adverb);
-                if (data.meaning.adverb=="") {
-                    $('#twinadverb').text("");
-                  }
-              
-            } catch (err) {
-              
-                $('#twinadverb').text("");
-              
-            }
-        });
-    
-        fetch("https://lexicala1.p.rapidapi.com/search?text="+s1, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
-                "x-rapidapi-host": "lexicala1.p.rapidapi.com"
-            }
-        })
-        .then(response => response.json()) 
-        .then(function(data){
-            console.log(data); 
-            //Clear
-            $('#lexicaladef').text("");
-            $('#lexicaladef2').text("");
-            //Insert Values
-            //Noun
-            var x = data.results;
-            var count=1;
-            for (i = 0; i <x.length; ++i) {
-                if (data.results[i].language=="en") {
-                    $('#lexicaladef').append(count+". ("+ data.results[i].headword.pos+") " +data.results[i].senses[0].definition+"<br>");
-                    count = count+1;
-                  }
-            }
-
-           
-        });
-    
-        fetch("https://bing-image-search1.p.rapidapi.com/images/search?q="+s1+"&count=40", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
-            "x-rapidapi-host": "bing-image-search1.p.rapidapi.com"
-        }
-        })
-        .then(response => response.json()) 
-        .then(function(data){
-            console.log(data); 
-            var i;
-            $('#img').attr("src", data.value[0].contentUrl);
-            for (i = 1; i < 30; i++) {
-                var x = "#img"+i
-                $(x).attr("src",data.value[i].contentUrl)
-            }
-        });
+        apicall();
     }
-
 })
-$('img').on("error", function() {
-    $(this).attr('src', placeholder.png);
-  });
+
+
+//function for all api calls
+function apicall(){
+    //get search query
+    var s1 = document.getElementById("search").value;
+    //change external search links to links with query
+    $("#google").attr("href",("https://www.google.com/search?q="+s1+"&oq="+s1+"aqs=chrome.0.69i59j46i131i433j0i433j69i60l3j69i61j69i60.701j0j4&sourceid=chrome&ie=UTF-8"))
+    $("#bing").attr("href",("https://www.bing.com/search?q="+s1+"&form=QBLH&sp=-1&pq=&sc=0-0&qs=n&sk=&cvid=E34CB66164EB416AAB0F4275867DC20C"))
+    $("#merweb").attr("href",("https://www.merriam-webster.com/dictionary/"+s1))
+    $("#oxf").attr("href",("https://www.oxfordlearnersdictionaries.com/definition/english/"+s1+"?q="+s1))
+    $("#duckduckgo").attr("href",("https://duckduckgo.com/?q="+s1+"&t=h_"))
+    $("#youtube").attr("href",("https://www.youtube.com/results?search_query="+s1))
+
+    //api call for lingua dict
+    fetch("https://lingua-robot.p.rapidapi.com/language/v1/entries/en/"+s1, {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
+        "x-rapidapi-host": "lingua-robot.p.rapidapi.com"
+    }
+    })
+    .then(response => response.json()) 
+    .then(function(data){
+        console.log(data); 
+        //clear
+        $('#linguadef').text("");
+        $('#linguadef2').text("");
+        $('#linguapro').text("");
+        $('#linguaexp').text("");
+        $('#linguaexp2').text("");
+        //Insert values
+        //html is already preset, so just add in text
+        //Pronounciation
+        try {
+
+            $('#linguapro').text(data.entries[0].pronunciations[0].context.regions[0]+": "+data.entries[0].pronunciations[0].transcriptions[0].transcription);
+            
+        } catch (err) {
+            
+            $('#linguapro').text("Pronounciation: Not found");
+            
+        }
+        //Definition 1
+        try {
+
+            $('#linguadef').text("1. (" +data.entries[0].lexemes[0].partOfSpeech+") "+data.entries[0].lexemes[0].senses[0].definition);
+            
+        } catch (err) {
+            
+            $('#linguadef').text("Example: Not found");
+            
+        }
+        //Example 1
+        try {
+
+            $('#linguaexp').text("Example: "+data.entries[0].lexemes[0].senses[0].usageExamples[0]);
+            
+        } catch (err) {
+            
+            $('#linguaexp').text("Example: Not found");
+            
+        }
+        //Definition 2
+        try {
+
+            $('#linguadef2').text("2. (" +data.entries[0].lexemes[1].partOfSpeech+") "+data.entries[0].lexemes[1].senses[0].definition);
+            
+        } catch (err) {
+            
+            $('#linguadef2').text("Definition: Not found");
+            
+        }
+        //Example 2
+        try {
+
+            $('#linguaexp2').text("Example. "+data.entries[0].lexemes[1].senses[0].usageExamples[0]);
+            
+        } catch (err) {
+            
+            $('#linguaexp2').text("Example: Not found");
+            
+        }
+
+    });
+
+    //api call for urban dictionary
+    fetch("https://mashape-community-urban-dictionary.p.rapidapi.com/define?term="+s1, {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
+        "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
+    }
+    })
+    .then(response => response.json()) 
+    .then(function(data){
+        console.log(data); 
+        //clear
+        $('#urbandef').text("");
+        $('#urbanexp').text("");
+        //Insert values
+        //html is already preset, so just add in text
+        //Definition 1
+        try {
+
+            $('#urbandef').text("1. " +data.list[0].definition);
+            
+        } catch (err) {
+            
+            $('#urbandef').text("Definition: Not found");
+            
+        }
+        //Example 1
+        try {
+
+            $('#urbanexp').text("Example: " +data.list[0].example);
+            
+        } catch (err) {
+            
+            $('#urbanexp').text("Example: Not found");
+            
+        }
+        //Definition 2
+        try {
+
+            $('#urbandef2').text("2. " +data.list[1].definition);
+            
+        } catch (err) {
+            
+            $('#urbandef2').text("Definition: Not found");
+            
+        }
+        //Example 2
+        try {
+
+            $('#urbanexp2').text("Example: " +data.list[1].example);
+            
+        } catch (err) {
+            
+            $('#urbanexp2').text("Example: Not found");
+            
+        }
+
+
+    });
+
+    //api call for twinword dictionary
+    fetch("https://twinword-word-graph-dictionary.p.rapidapi.com/definition/?entry="+s1, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
+            "x-rapidapi-host": "twinword-word-graph-dictionary.p.rapidapi.com"
+        }
+    })
+    .then(response => response.json()) 
+    .then(function(data){
+        console.log(data); 
+        //clear elements
+        $('#twindef').empty();
+        
+        //insert values
+        //append html code as a child elemnt
+        var definitioncount=1;
+        //Noun
+        try {
+            var noun = (data.meaning.noun).split("(nou)");
+            if (data.meaning.noun!="") {
+                $('#twindef').append("<a>"+definitioncount+". (nou) " +noun[1]+"</a><br>");
+                definitioncount+=1;
+            }
+            
+        } catch (err) {
+            
+            $('#twindef').append("<a>Definition: Not found</a><br>");
+            
+        }
+        //Verb
+        try {
+            var verb = (data.meaning.verb).split("(vrb)");
+            if (data.meaning.verb!="") {
+                $('#twindef').append("<a>"+definitioncount+". (vrb)" +verb[1]+"</a><br>");
+                definitioncount+=1;
+                }
+            
+        } catch (err) {
+            
+            
+            
+        }
+        //Adj
+        try {
+            var adj = (data.meaning.adjective).split("(adj)");
+            
+            if (data.meaning.adjective!="") {
+                $('#twindef').append("<a>"+definitioncount+". (adj)" +adj[1]+"</a><br>");
+                definitioncount+=1;
+                }
+            
+        } catch (err) {
+            
+            
+        }
+        //Adv
+        try {
+            var adverb = (data.meaning.adverb).split("(adv)");
+            
+            if (data.meaning.adverb!="") {
+                $('#twindef').append("<a>"+definitioncount+". (adv)" +adverb[1]+"</a><br>");
+                definitioncount+=1;
+                }
+            
+        } catch (err) {
+            
+            
+        }
+    });
+
+    //api call for lexicala
+    fetch("https://lexicala1.p.rapidapi.com/search?text="+s1, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
+            "x-rapidapi-host": "lexicala1.p.rapidapi.com"
+        }
+    })
+    .then(response => response.json()) 
+    .then(function(data){
+        console.log(data); 
+        //Clear
+        $('#lexicaladef').empty();
+        //Insert Values as child html
+        //Noun
+        var x = data.results;
+        var count=1;
+        
+        for (i = 0; i <x.length; ++i) {
+            if (data.results[i].language=="en") {
+                $('#lexicaladef').append("<a>"+count+". ("+ data.results[i].headword.pos+") " +data.results[i].senses[0].definition+"</a><br>");
+                count = count+1;
+                }
+        }
+        if (count-1==0){
+            $('#lexicaladef').append("<a>Definition: Not found</a><br>");
+            }
+
+        
+    });
+
+    //api call for bing image search
+    fetch("https://bing-image-search1.p.rapidapi.com/images/search?q="+s1+"&count=40", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
+        "x-rapidapi-host": "bing-image-search1.p.rapidapi.com"
+    }
+    })
+    .then(response => response.json()) 
+    .then(function(data){
+        console.log(data); 
+        var i;
+        //for first, predfined image html (could have went without it also, left it for testing purposes)
+        $('#img').attr("src", data.value[0].contentUrl);
+        //for subsequent images
+        for (i = 1; i < 30; i++) {
+            var x = "#img"+i
+            $(x).attr("src",data.value[i].contentUrl)
+        }
+    });
+    
+}
+
+
+
+//autocomplete suggestion feature
+$('#search').keyup(function(e){
+    //get query
+    var s1 = document.getElementById("search").value;
+    //api call for top searches webit
+    fetch("https://webit-keyword-search.p.rapidapi.com/autosuggest?q="+s1+"&language=en", {
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "adeda04aaamsh0df4721c82d6031p18c740jsn1a7ea2e6cbb4",
+        "x-rapidapi-host": "webit-keyword-search.p.rapidapi.com"
+    }
+    })
+    .then(response => response.json()) 
+    .then(function(data){
+        //clear
+        $('#suggestions').empty();
+        var arr = [];
+        //append html as child element
+        for (i = 0; i <5; ++i) {
+            $('#suggestions').append('<div class="suggestion-content">'+data.data.results[i]+'</div>')   
+            arr.push(data.data.results[i]);
+            
+        }
+    //on click, search the query
+    $('.suggestion-content').mousedown(function(e){
+        var s2= this.innerHTML;
+        console.log(s2);
+        $('#search').val(s2);
+        
+        apicall();
+        
+        })
+        
+    })
+    .catch(err => {
+        console.error(err);
+    });
+})
+//on focus out, clear suggestions to empty view
+$('#search').focusout(function(){
+    $('#suggestions').empty();
+})
+//load other 29 images
 $( document ).ready(function() {
-    var error= "onerror=this.src='placeholder.png'";
+    var error= "onerror=this.src='Images/placeholder.png'";
     for (i = 1; i < 30; i++) {
-        $('.images').append("<img id='img"+(i)+"' src='placeholder.png' class='col-2' "+error+" ></img>");
+        $('.images').append("<img id='img"+(i)+"' src='placeholder.png' class='col-2 imgs' "+error+" ></img>");
 
     }
 });
-
